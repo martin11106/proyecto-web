d2422db436d37c7a8659ab112bd24451856806f4
DaniGuerra <daniguerra13@gmai.com>
2018-11-05 23:57:13 -0600

    cree answerController
    lo que dice arriba

---
+++ app/Http/Controllers/answerController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class answerController extends Controller
+{
+
+    //recibe como parametro una id de pregunta y regresa todas las respuestas asociadas a esa id
+    public function listaAnswer(){
+
+    }
+
+
+    //recibe como parametro una id de pregunta y hace una respuesta asociada a esa id
+    public function hacerAsnwer(){
+
+    }
+
+    //recibe como parametro una id de respuesta y le asigna una calificacion
+    public function calificarAnswer(){
+
+    }
+
+
+    //recibe como parametro una id de respuesta y borra la respuesta
+    public function deleteAnswer(){
+
+    }
+
+    //recibe como parametro una id de respuesta y reporta al usuario que hizo la pregunta
+    public function reportAnswer(){
+
+    }
+
+
+}
