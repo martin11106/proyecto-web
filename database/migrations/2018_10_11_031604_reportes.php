e1a3ddec81bbbabbaa038f013d62069712f6c9c9
Ernesto-Guerra <173216@ids.upchiapas.edu.mx>
2018-11-22 18:56:12 -0600

    Mejorado y recargado

--- database/migrations/2018_10_11_031604_reportes.php
+++ database/migrations/2018_10_11_031604_reportes.php
@@ -17,7 +17,7 @@ class Reportes extends Migration
             Schema::defaultStringLength(191); 
             $table->increments('id');
             $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('user');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->integer('answare_id')->unsigned();
             $table->foreign('answare_id')->references('id')->on('answare');
             $table->string('descripcion');
--- routes/web.php
+++ routes/web.php
@@ -14,37 +14,31 @@
 Route::get('/', function () {/* la diagonal es la raiz y es lo que aparece primero */
     return view('create');
 });
-Route::get('/blog', function (){
+
+Route::get('/blog', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
 
 Route::get('/t', function (){
     return view('ranking');
 });
 
 
-=======
-route::get('/test',function(){
+Route::get('/test',function(){
     return view('test');
 });
 
->>>>>>> 7e08d0dd91da01677f6d2c545c8d18375659067f
 Route::post('/usuario','userController@guardar');
-
-Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/reportes','reportesController@index');  //yaaa
 Route::post('/reportes','reportesController@store'); //yaaaaaa
 Route::put('/reportes/{id}','reportesController@update'); //yaaaaa
 Route::delete('/reportes/{id}','reportesController@destroy');//yaaaaaaaaa
-<<<<<<< HEAD
 Route::get('/reportes/{id}','reportesController@show');//yaaaaa   
 
 Route::resource('user', 'userController',['except'=>'show']);
-=======
 Route::get('/reportes/{id}','reportesController@show');//yaaaaa
 
 //---Hacer Preguntas ---
@@ -52,5 +46,15 @@ Route::get('/reportes/{id}','reportesController@show');//yaaaaa
 Route::apiResource('/makeQuestion','makeQuestionController');
 
 
-//----------------------
->>>>>>> 7e08d0dd91da01677f6d2c545c8d18375659067f
+//---------------------------------------------------------
+// AQUI EMPEZÓ A TRABAJAR EL GRAN Y PODEROSO SMARTNETO
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::post('/saveuser','userController@guardar');
+
+Route::post('login','Auth\LoginController@login')->name('login');
+
+Route::get('/logout','Auth\LoginController@logout')->name('logout');
