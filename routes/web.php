271a39e8c86ff199cd29ff41200e2c2226d30f92
martin11106 <martin-adonis@hotmail.es>
2018-11-26 13:55:49 -0600

    actualizacion

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {/* la diagonal es la raiz y es lo que aparece prime
 });
 
 Route::get('/blog', function () {
-    return view('login');
+    return view('register');
 });
 
 
